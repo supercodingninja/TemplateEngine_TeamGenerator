62042914be9db0acbf7adad8c5eec299dbb1cb02
SCN <supercodingninja@outlook.com>
2021-02-22 22:09:17 -0800

    Completed const intQ

--- Develop/lib/q.js
+++ Develop/lib/q.js
@@ -1,7 +1,8 @@
-// Ref. https://github.com/SBoudrias/Inquirer.js/ //
+// Query prompts Ref. https://github.com/SBoudrias/Inquirer.js/ //
 
 const inquirer = require('inquirer');
 const emailValidator = require('email-validator');
+const { Validator } = require('node-input-validator');
 
 // Questions to ask the user. //
 class userInquiries {
@@ -11,59 +12,227 @@ class userInquiries {
         this.questions = [
 
             {
-                type: 'input',
+                type: 'list',
+                
                 name: 'name',
+
                 message: `What ${this.role} do you wish to add?`,
+                
+                choices: ['Intern', 'Employee', 'Engineer', 'Manager']
 
-                validate: function (value) {
-
-                    // Ref. https://github.com/supercodingninja/passwordGenerator/blob/main/pw.js, and see if there is an easier way to write this. // 
-                    var pass = value.match('~!@#$%^&*?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ'.split(''));
-
-                    if (pass) { return true }
-                    return `A valid name does not include integers or special characters.`;
-                }
             },
-
+            
             {
                 type: 'input',
+                
                 name: 'id',
-                message: `State ${this.role} ID number?`,
-                validate: function (value) {
-                    var pass = value.match('~!@#$%^&*?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ'.split(''));
-                    if (pass) { return true }
-                    return `Please enter a valid employee ID number`
-                }
+
+                    validate: function (value) {
+
+                        // Ref. https://www.npmjs.com/package/node-input-validator //
+                        var pass = value.match('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ'.split(''));
+                        
+                        if (pass) { return true };
+                        
+                        return `Please enter a valid employee ID number`
+                    },
+                
+                message: `State ${this.role} ID number?`
             },
 
             {
                 type: 'input',
+                
                 name: 'email',
-                message: `What is the email address for this ${this.role}?`,
-                validate: emailValidator
+                
+                    // Ref. https://www.npmjs.com/package/email-validator //
+                    validate: emailValidator,
+                
+                    message: `What is the email address for this ${this.role}?`,
             },
 
             {
                 type: 'list',
-                name: 'addMember',
+                
+                name: 'memStat',
+                
                 message: `Select which team member you would like to add.`,
-                choices: ['Engineer', 'Intern', 'Employee', 'No more.']
+                
+                choices: ['No additional members.', 'Intern', 'Employee', 'Engineer', 'Manager']
             }
-        ]
-        
+        ],
+
         this.role = role
-
-
-
     }
+
     nextQuery(question) {
 
-        this.questions.splice(3, 0, question)
+        this.questions.splice(5, 0, question)
     };
 
     nextQuery() {
 
         return inquirer.prompt(this.questions)
     }
-
 };
+
+
+// Some roles will have different requirements.  Below is a simplified method, rather than using long conditional statements. //
+const intQ = [
+    {
+    
+        type:'list',
+        
+        name:'GitHub',
+        
+        message:'Does the intern have a GitHub profile?',
+
+        choices: ['No.','Yes.' = [
+            
+                {
+                
+                    type: 'input',
+                        
+                    name: 'name',
+                        validate: function (value) {
+
+                            // Ref. https://www.npmjs.com/package/node-input-validator //
+                            var pass = value.match(Validator);
+                            
+                            if (pass) {return true};
+                            
+                            return `Please enter a valid user name.`
+                        },
+
+                    message: `Please enter the GitHub user name for the intern.`
+                }
+            ]
+        ]
+    },
+
+    {
+    
+        type:'list',
+        
+        name:'GitHub',
+        
+        message:'Does the intern attend an university?',
+
+        choices: ['No.','Yes.' = [
+                
+                {
+            
+                    type: 'input',
+                        
+                    name: 'name',
+                        validate: function (value) {
+
+                            // Ref. https://www.npmjs.com/package/node-input-validator //
+                            var pass = value.match(Validator);
+                            
+                            if (pass) {return true};
+                            
+                            return `Please enter a valid university.`
+                        },
+
+                    message: `Please enter the university the intern attends.`
+                }
+            ]
+        ]
+    },
+
+    {
+        type:'list',
+    
+        name:'intFutRol',
+    
+        message:'Does the intern desire to obtain a future role with your company?',
+
+        choices: ['No future role desired.', 'Yes' = [
+            {
+                type: 'list',
+                
+                name: 'futRole',
+                
+                message: `Select the desire role the intern desires.`,
+                
+                choices: ['Employee', 'Engineer', 'Manager']
+            }
+        ]]
+    }
+];
+
+
+const empQ = [
+    {
+    
+    type:'input',
+    
+    name:'GitHub',
+    
+    message:'Please supply the GitHub username for your engineer.',
+
+    validate: function (value) {
+
+        // Ref. https://www.npmjs.com/package/node-input-validator //
+        var pass = value.match(Validator);
+        
+        if (pass) {return true};
+        
+        return `Please enter a valid user name.`
+        }
+    },
+
+    {
+        type:'list',
+    
+        name:'tenure',
+    
+        message:'Please state the tenure of the engineer.',
+
+        choices:
+    }
+];
+
+
+const engQ = [
+    {
+    
+    type:'input',
+    
+    name:'GitHub',
+    
+    message:'Please supply the GitHub username for your engineer.',
+    },
+
+    {
+        type:'input',
+    
+        name:'GitHub',
+    
+        message:'Please supply the GitHub username for your engineer.',
+    }
+];
+
+
+const mgrQ = [
+    {
+    
+    type:'input',
+    
+    name:'GitHub',
+    
+    message:'Please supply the GitHub username for your engineer.',
+    },
+
+    {
+        type:'input',
+    
+        name:'GitHub',
+    
+        message:'Please supply the GitHub username for your engineer.',
+    }
+];
+
+module.exports = {intQ, empQ, engQ, mgrQ};
+
