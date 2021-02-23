501c02a4096ee5686d6604239f06520ef9dec840
SCN <supercodingninja@outlook.com>
2021-02-22 18:23:01 -0800

    I cannot put a method in my array of objects.

--- Develop/lib/q.js
+++ Develop/lib/q.js
@@ -1,24 +1,25 @@
 // Ref. https://github.com/SBoudrias/Inquirer.js/ //
 
 const inquirer = require('inquirer');
+const emailValidator = require('email-validator');
 
 // Questions to ask the user. //
 class userInquiries {
-    
+
     constructor(role) {
-        
+
         this.questions = [
-            
+
             {
                 type: 'input',
                 name: 'name',
                 message: `What ${this.role} do you wish to add?`,
-                
+
                 validate: function (value) {
-                    
+
                     // Ref. https://github.com/supercodingninja/passwordGenerator/blob/main/pw.js, and see if there is an easier way to write this. // 
-                    var pass = value.match([abcdefghijklmnopqrstuvwxyz], [ABCDEFGHIJKLMNOPQRSTUVWUXYZ]);
-                    
+                    var pass = value.match('~!@#$%^&*?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ'.split(''));
+
                     if (pass) { return true }
                     return `A valid name does not include integers or special characters.`;
                 }
@@ -39,12 +40,7 @@ class userInquiries {
                 type: 'input',
                 name: 'email',
                 message: `What is the email address for this ${this.role}?`,
-                validate: function (value) {
-                    var pass = value.match('~!@#$%^&*?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWUXYZ'.split(''));
-
-                    if (pass) { return true }
-                    return `Please enter a valid email address`;
-                }
+                validate: emailValidator
             },
 
             {
@@ -52,15 +48,22 @@ class userInquiries {
                 name: 'addMember',
                 message: `Select which team member you would like to add.`,
                 choices: ['Engineer', 'Intern', 'Employee', 'No more.']
-            },
+            }
+        ]
+        
+        this.role = role
 
-        this.role = role;
 
-    nextQuery(question) {
-        this.questions.splice(3, 0, question);
+
     }
+    nextQuery(question) {
+
+        this.questions.splice(3, 0, question)
+    };
 
     nextQuery() {
-        return inquirer.prompt(this.questions);
+
+        return inquirer.prompt(this.questions)
     }
-        ];
+
+};
