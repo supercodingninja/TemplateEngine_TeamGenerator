91274c1e82b7575b4c7faa5fbb9501a50b9e01c5
SCN <supercodingninja@outlook.com>
2021-02-23 04:33:27 -0800

    Removed criterea hint comments.

--- Develop/app.js
+++ Develop/app.js
@@ -201,19 +201,3 @@ function renderToHTML() {
         console.log('team.HTML file has been created, successfully; and is located in the `output` directory.');
     });
 };
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
